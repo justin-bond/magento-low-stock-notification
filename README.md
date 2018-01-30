023279c99b731f6c79c924f020d777fe6db1243d
Justin Bond <bond.justink@gmail.com>
2018-01-30 10:02:39 -0800

    Added functionality, edited Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # magento low stock notification
+
+When a product is saved or purchased, checks if the stock is less than the notify stock quantity and sends a low stock email, if the stock is 0 it will send an out of stock email. 
+
+From and To email addresses need to be added in module.
--- app/code/local/Justinkbond/LowStockNotification/Model/Observer.php
+++ app/code/local/Justinkbond/LowStockNotification/Model/Observer.php
@@ -4,6 +4,42 @@ class Justinkbond_LowStockNotification_Model_Observer
 {
     public function lowStockReport($observer)
     {
-        
+        $event = $observer->getEvent();
+        $stockItem = $event->getItem();
+
+        if ($stockItem->getQty() < $stockItem->getNotifyStockQty()) {
+            //stock is lower than the notify amount, send email
+            $product = Mage::getModel('catalog/product')->load($stockItem->getProductId());
+
+            $body = "";
+
+            if ($stockItem->getQty() > 0) {
+                $subject = "[Notice] Low Stock Notification"; // subject text
+
+                $body .= "{$product->getName()} :: {$product->getSku()} is low on stock!\n\n";
+                $body .= "Current Qty: {$stockItem->getQty()}\n";
+                $body .= "Low Stock Date: {$stockItem->getLowStockDate()}\n";
+            } else {
+                $subject = "[Notice] Out of Stock Notification"; // subject text
+
+                $body .= "{$product->getName()} :: {$product->getSku()} is out of stock!\n\n";
+                $body .= "Out of Stock Date: {$stockItem->getLowStockDate()}\n";
+            }
+
+            $fromEmail = ""; // sender email address
+            $toEmail = ""; // recipient email address 
+            // $ccEmail = ""; // cc recipient email address 
+            
+            $mail = new Zend_Mail();        
+            $mail->setBodyText($body);
+            $mail->setFrom($fromEmail);
+            $mail->addTo($toEmail);
+            if ($ccEmail) {
+                $mail->addCc($ccEmail);
+            }
+            $mail->setSubject($subject);
+            $mail->send();
+            
+        }
     }
 }
